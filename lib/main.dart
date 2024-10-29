6de7313eb7c86c21997408b278f6cf441b3c8837
Khadiga Abdelrahman <khadija15599@gmail.com>
2024-10-30 01:41:13 +0300

    integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(TodoApp());
 }
 
@@ -16,6 +19,8 @@ class TodoApp extends StatelessWidget {
       },
       initialRoute: HomeScreen.routeName,
       theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
